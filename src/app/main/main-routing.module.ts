bce1823e9c842121022f192ff69896c0019a3fdb
GEVIC21 <ndilbedouvic@gmail.com>
2024-07-07 14:34:34 +0000

    sidebar

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MessageComponent } from './pages/message/message.component';
+import { PublicationComponent } from './pages/publication/publication.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 const routes: Routes = [
   {path:'',component:MainComponent,
     children:[
        {path:'',redirectTo:'/home',pathMatch:'full'},
        {path:'home',component:HomeComponent},
+       {path:'message',component: MessageComponent},
+       {path:'publication',component:PublicationComponent},
+       {path:'setting',component:SettingsComponent},
       
-    ]}
+    ]},
+    
 ];
 
 @NgModule({
