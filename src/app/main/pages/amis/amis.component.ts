f3c6e1ed52542e081140ec25867f31958d1d97e8
GEVIC21 <ndilbedouvic@gmail.com>
2024-07-09 22:35:59 +0000

    Section Amis

---
+++ src/app/main/pages/amis/amis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amis',
+  templateUrl: './amis.component.html',
+  styleUrls: ['./amis.component.css']
+})
+export class AmisComponent {
+
+}
